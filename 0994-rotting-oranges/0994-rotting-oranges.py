f691e7e69f316afb4ca830b2092e907ffdf2f727
✦ 𝒂𝒕𝒉𝒂𝒓𝒗.𝒅𝒆𝒗 <144611888+codedbyatharv@users.noreply.github.com>
2026-07-03 09:34:30 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.93%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid):
+        rows, cols = len(grid), len(grid[0])
+        q = deque()
+        fresh = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+
+        if fresh == 0:
+            return 0
+
+        time = 0
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while q and fresh:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        q.append((nr, nc))
+
+            time += 1
+
+        return time if fresh == 0 else -1
